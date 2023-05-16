edded28ecb6db02b9e22e4ace12c090d12984293
AbayomiAroyeun <aroyeunmichael@gmail.com>
2023-05-16 14:14:35 -0400

    models created

---
+++ client/src/components/TravelGo.js
@@ -0,0 +1,12 @@
+import React from react;
+const TravelGo = () => {
+return (
+<div>
+    <h1>Welcome to TravelGo!</h1>
+    <p>Make your prestigious Vaccation with ease.</p>
+</div>
+
+);
+};
+
+export default TravelGo;
